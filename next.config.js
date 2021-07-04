74f54716ce79aae86bfab6d7510ae8b49bd333fb
Kanchana Sondee <kangana512@gmail.com>
2021-07-04 23:11:37 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
+const withPreact = require("next-plugin-preact");
+
+const configs = {
+  image: {
+    domains: ['*'],
+  },
 }
+
+module.exports = withPreact({
+  reactStrictMode: true,
+  ...configs
+})
